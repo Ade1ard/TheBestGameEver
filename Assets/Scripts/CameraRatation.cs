8c1c37600482ad8f5249d6537a3283172ed40d8c
Ade1ard <gorbunovjarroslav@gmail.com>
2024-02-07 17:15:01 +0300

    ver 2(magic tolic x y)

--- Assets/Scripts/CameraRatation.cs
+++ Assets/Scripts/CameraRatation.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class CameraRatation : MonoBehaviour
 {
-    public float RotatoinSpeed;
+    public Transform CameraAxisTransform;
+    public float RotatinSpeedY;
+    public float RotatinSpeedX;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,7 @@ public class CameraRatation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotatoinSpeed * Input.GetAxis("Mouse X"), 0);
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotatinSpeedX * Input.GetAxis("Mouse X"), 0);
+        CameraAxisTransform.localEulerAngles = new Vector3(CameraAxisTransform.localEulerAngles.x + Time.deltaTime * RotatinSpeedY * Input.GetAxis("Mouse Y"), 0, 0);
     }
 }
