c796a066601dd92be451844939e93be2f9b2f48c
Ade1ard <gorbunovjarroslav@gmail.com>
2024-02-07 17:46:34 +0300

    final camera ratation

--- Assets/Scripts/CameraRatation.cs
+++ Assets/Scripts/CameraRatation.cs
@@ -23,6 +23,8 @@ public class CameraRatation : MonoBehaviour
         transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotatinSpeedX * Input.GetAxis("Mouse X"), 0);
 
         var newAngelX = CameraAxisTransform.localEulerAngles.x - Time.deltaTime * RotatinSpeedY * Input.GetAxis("Mouse Y");
+        if (newAngelX > 180)
+            newAngelX -= 360;
         newAngelX = Mathf.Clamp(newAngelX, MinAngel, MaxAngel);
 
         CameraAxisTransform.localEulerAngles = new Vector3(newAngelX, 0, 0);
