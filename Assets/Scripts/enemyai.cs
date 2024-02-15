e82bca356b6c6147eacd567e58d2f200a2d9afeb
Ade1ard <gorbunovjarroslav@gmail.com>
2024-02-15 19:03:43 +0300

    smart enemy

---
+++ Assets/Scripts/enemyai.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyai : MonoBehaviour
+{
+    public List<Transform> targetpoints;
+    public PlayerController player;
+    public float viewAngle;
+
+    private NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        PickNewTargetPoint();
+    }
+
+    void Update()
+    {
+        if (_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewTargetPoint();
+        }
+
+        var direction = player.transform.position - transform.position;
+
+        _isPlayerNoticed = false;
+
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+        
+    }
+    private void PickNewTargetPoint()
+    {
+        _navMeshAgent.destination = targetpoints[Random.Range(0, targetpoints.Count)].position;
+    }
+}
