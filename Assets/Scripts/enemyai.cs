db64af7d182254c1813120e77068ff0afb19a136
Ade1ard <gorbunovjarroslav@gmail.com>
2024-02-29 19:54:49 +0300

    hp my and enemy

--- Assets/Scripts/enemyai.cs
+++ Assets/Scripts/enemyai.cs
@@ -8,9 +8,11 @@ public class enemyai : MonoBehaviour
     public List<Transform> targetpoints;
     public PlayerController player;
     public float viewAngle;
+    public float damage = 20;
 
     private NavMeshAgent _navMeshAgent;
     private bool _isPlayerNoticed;
+    private playerHealt _playerHealt;
 
     public Animator _animatorEnemy;
 
@@ -19,13 +21,15 @@ public class enemyai : MonoBehaviour
         _navMeshAgent = GetComponent<NavMeshAgent>();
 
         PickNewTargetPoint();
+
+        _playerHealt = player.GetComponent<playerHealt>();
     }
 
     void Update()
     {
         if (!_isPlayerNoticed)
         {
-            if (_navMeshAgent.remainingDistance == 0)
+            if (_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
             {
                 PickNewTargetPoint();
             }
@@ -48,11 +52,15 @@ public class enemyai : MonoBehaviour
             }
         }
 
+        
+
         if (_isPlayerNoticed)
         {
             _navMeshAgent.destination = player.transform.position;
         }
-        
+
+        AttackUpdate();
+
         if (_navMeshAgent.isStopped == false)
         {
             _animatorEnemy.SetBool("is run", true);
@@ -66,5 +74,16 @@ public class enemyai : MonoBehaviour
     private void PickNewTargetPoint()
     {
         _navMeshAgent.destination = targetpoints[Random.Range(0, targetpoints.Count)].position;
+    }
+
+    private void AttackUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            if(_navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                _playerHealt.DealDamage(damage * Time.deltaTime);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/playerHealt.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealt : MonoBehaviour
+{
+    public float value = 100;
+    public RectTransform valueRectTransform;
+
+    private float _maxValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _maxValue = value;
+
+        DrawHealtBar();
+    }
+
+    public void DealDamage(float damage)
+    {
+        value -= damage;
+        if(value <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        DrawHealtBar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DrawHealtBar()
+    {
+        valueRectTransform.anchorMax = new Vector2(value / _maxValue, 1);
+    }
+ 
+}
