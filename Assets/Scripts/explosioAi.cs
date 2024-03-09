d964ff07334c120cf47c17274ca11495b7085014
Ade1ard <gorbunovjarroslav@gmail.com>
2024-03-09 18:15:23 +0300

    last

---
+++ Assets/Scripts/explosioAi.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosioAi : MonoBehaviour
+{
+    public float damage = 50;
+    public float maxSize = 5;
+    public float speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += Vector3.one * Time.deltaTime * speed;
+
+        if(transform.localScale.x > maxSize)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var playerHealt = other.GetComponent<playerHealt>();
+        if (playerHealt != null)
+        {
+            playerHealt.DealDamage(damage);
+        }
+
+        var enemyHealt = other.GetComponent<enemyhealt>();
+        if (enemyHealt != null)
+        {
+            enemyHealt.DealDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/granade.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class granade : MonoBehaviour
+{
+    public float delay = 3;
+    public GameObject explasionPrefab;
+    // Start is called before the first frame update
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Invoke("Explosion", 3);
+    }
+
+    private void Explosion()
+    {
+        Destroy(gameObject);
+        var explosion = Instantiate(explasionPrefab);
+        explosion.transform.position = transform.position;
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
