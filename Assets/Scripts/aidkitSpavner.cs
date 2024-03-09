8197e03f287841f40637999c033af91ac3be3a2f
Ade1ard <gorbunovjarroslav@gmail.com>
2024-03-09 21:19:03 +0300

    apteqkaept

---
+++ Assets/Scripts/aidkitSpavner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aidkitSpavner : MonoBehaviour
+{
+    public apteqkaAI apteqkaPrefab;
+    public float delaymin = 10;
+    public float delaymax = 25;
+
+    public List<Transform> spawnPoints;
+
+    private apteqkaAI _aidkit;
+
+    private void Update()
+    {
+        if (_aidkit != null) return;
+        if (IsInvoking()) return;
+        Invoke("CreateAidkit", Random.Range(delaymin, delaymax));
+    }
+
+    private void CreateAidkit()
+    {
+        _aidkit = Instantiate(apteqkaPrefab);
+        _aidkit.transform.position = spawnPoints[Random.Range(0, spawnPoints.Count)].position;
+    }
+
+}
